d6fb05046c2962ad43d2a6aaa6990a50f7b5c201
Blockhead <jbis1337@hotmail.com>
2023-02-21 04:27:55 +1100

    Run the mod code after all the other mods

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
-minetest.override_item("default:stonebrick", {
-	drawtype = "mesh",
-	tiles = {"meshbrick.png"},
-	mesh = "meshbrick.obj"
-})
+minetest.register_on_mods_loaded(function()
+	minetest.override_item("default:stonebrick", {
+		drawtype = "mesh",
+		tiles = {"meshbrick.png"},
+		mesh = "meshbrick.obj"
+	})
+end
+)
